c4b32a7b68dd2607354f32321f86407a43542ae7
IT21489914 <99627088+IT21489914@users.noreply.github.com>
2022-02-25 01:09:36 +0000

    Completed the codes

--- exercise02.cpp
+++ exercise02.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 using namespace std;
-
+//Function prototype
 int volume(int height, int width, int length);
 
 int main() {
-    int box1Height, box1Width, box1Length;
+    int box1Height, box1Width, box1Length;//Declaring variables
     int box2Height, box2Width, box2Length;
     int totalVolume, totalSurface;
     
-    cout << "Enter Box 1 Height : ";
-    cin >> box1Height;
-    cout << "Enter Box 1 Width : ";
-    cin >> box1Width;
-    cout << "Enter Box 1 Length : ";
-    cin >> box1Length;
+    cout << "Enter Box 1 Height : ";//Input Box1 height
+    cin >> box1Height;//Reads Box1 height
+    cout << "Enter Box 1 Width : ";//Input Box1 width
+    cin >> box1Width;//Reads Box1 width
+    cout << "Enter Box 1 Length : ";//Input Box1 length
+    cin >> box1Length;//Reads Box1 length
     
-     cout << "Enter Box 2 Height : ";
-    cin >> box2Height;
-    cout << "Enter Box 2 Width : ";
-    cin >> box2Width;
-    cout << "Enter Box 2 Length : ";
-    cin >> box2Length;
-    
+    cout << "Enter Box 2 Height : ";//Input Box2 height
+    cin >> box2Height;//Reads Box2 height
+    cout << "Enter Box 2 Width : ";//Input Box2 width
+    cin >> box2Width;//Reads Box2 width
+    cout << "Enter Box 2 Length : ";//Input Box2 length
+    cin >> box2Length;//Reads Box2 length
+    //Calculate totalvolume
     totalVolume = volume(box1Height, box1Width, box1Length)
              + volume(box2Height, box2Width, box2Length);
-             
+    //Displays total volume         
     cout << "Volume of Box is " << totalVolume << endl;
     
     return 0;
 }
 
-// Implement the Volume() function here
+// Implement the Volume() function 
+int volume(int height, int width, int length)
+{
+  return height * width * length;
+}
--- exercise04.cpp
+++ exercise04.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void print(int len, int wth);
-void input(int len, int wth);
+void print(int len, int wth);//Function prototype
+void input(int &len, int &wth);
 
 // Do not change the main() function
 int main() {
-   int length = 10, width = 5;
-   input(length, width);
+   int length = 10, width = 5;//Initializing variables
+   input(length, width);//Function calling
    print(length, width);
    return 0;
 }
 
 // Do not change the print() function
-void print(int len, int wth) {
+void print(int len, int wth) {//Function implementation
    cout << "Length : " << len 
         << ", Width  : " << wth << endl;
 }
 
 // Implement the Input Function here
+void input(int &len, int &wth)
+{
+  cout << "Enter length ";
+  cin >> len;
+  cout << "Enter width ";
+  cin >> wth;
+  
+}
